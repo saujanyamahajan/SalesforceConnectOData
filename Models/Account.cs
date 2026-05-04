67a542d564a66bccd56d3dd8f8baa6eb7596d4ac
saujanyamahajan <saujanya.9919@gmail.com>
2026-05-04 23:22:33 +0530

    Resolved merge conflict

---
+++ Models/Account.cs
@@ -0,0 +1,13 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace SalesforceConnectOData.Models;
+
+public class Account
+{
+    [Key]
+    public int Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public string? Email { get; set; }
+    public string? Phone { get; set; }
+    public DateTime CreatedAt { get; set; }
+}
