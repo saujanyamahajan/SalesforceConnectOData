d7aa756e5ab84517db77df7b9db34428d7bd5d8b
saujanyamahajan <saujanya.9919@gmail.com>
2026-06-07 10:30:00 +0530

    setting max upload limit to 5000

--- Program.cs
+++ Program.cs
@@ -7,22 +7,26 @@ using SalesforceConnectOData.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// EF Core → SQL Server
 builder.Services.AddDbContext<AppDbContext>(opts =>
     opts.UseSqlServer(builder.Configuration.GetConnectionString("Default")));
 
-// Build OData EDM model
 var odataBuilder = new ODataConventionModelBuilder();
-odataBuilder.EntitySet<Account>("Accounts");
 
-// Register OData
+
+// Configure Assets - full CRUD as well
+
 builder.Services.AddControllers()
     .AddOData(opts =>
-        opts.Select().Filter().OrderBy().Expand().Count().SetMaxTop(1000)
+        opts.Select()
+            .Filter()
+            .OrderBy()
+            .Expand()
+            .Count()
+            .SetMaxTop(5000)
             .AddRouteComponents("odata", odataBuilder.GetEdmModel()));
 
 var app = builder.Build();
-app.UseMiddleware<ApiKeyMiddleware>();
+
 app.MapControllers();
 app.Run();
 
